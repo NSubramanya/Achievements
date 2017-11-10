26ec4867e110be8c181b13b066a3f340019bfe19
NSubramanya <subramanya.n@solcen.in>
2017-11-10 17:26:45 +0530

    Added basic UI tests

--- achivementUITests/achivementUITests.swift
+++ achivementUITests/achivementUITests.swift
@@ -9,7 +9,8 @@
 import XCTest
 
 class achivementUITests: XCTestCase {
-        
+    var app: XCUIApplication!
+    
     override func setUp() {
         super.setUp()
         
@@ -18,8 +19,10 @@ class achivementUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
+        app = XCUIApplication()
+        //XCUIApplication().launch()
+        //app.launchArguments.append("--uitesting")
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
@@ -28,9 +31,43 @@ class achivementUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testFinalVCNavigation() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        app.launch()
+        
+        app.tables.cells.firstMatch.tap()
+        app.tables.cells.firstMatch.tap()
+        app.tables.cells.firstMatch.tap()
+        
+        //let firstcell = app.tableRows.firstMatch
+        
+        XCTAssert(app.staticTexts["Name"].exists)
+    }
+    
+    func testFullNavigation() {
+        app.launch()
+        
+        app.tables.cells.firstMatch.tap()
+        //app.tableRows.firstMatch.tap()
+        app.tables.cells.firstMatch.tap()
+        app.tables.cells.firstMatch.tap()
+        
+        app.navigationBars.buttons.element(boundBy: 0).tap()
+        app.navigationBars.buttons.element(boundBy: 0).tap()
+        app.navigationBars.buttons.element(boundBy: 0).tap()
+        
+        XCTAssert(userInFirstVC())
+    }
+    
+    func userInFirstVC() -> Bool {
+        let titleText = app.navigationBars.element.identifier
+        if titleText == "Groups" {
+            return true
+        } else {
+            return false
+        }
     }
     
 }
+
